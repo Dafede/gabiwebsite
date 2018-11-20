04bf14e0e9adbb5a6ed120e10e8e92d953fed9c5
Kensousen <dafede333@gmail.com>
2018-11-20 20:28:57 +0100

    Refreshing...

--- js/main.js
+++ js/main.js
@@ -31,6 +31,42 @@ $('#avatar').affix({
         .children('.photo').css({'background-image': 'url('+ $(this).attr('data-image') +')'});
     })
 
+	
+
+
+$( ".col-main-index" ).hover(
+  function() {
+      $(this).find("div").animate({
+    height: "2.5em",
+	paddingTop: "0.5em"
+  }, 500, function() { });
+  }, function() {
+     $(this).find("div").animate({
+    height: "2em",
+	paddingTop: "0.25em"
+  }, 500, function() { });
+  }
+);
+
+
+
+window.setTimeout(function() {
+    $(".dropdown-menu-title").css('visibility', 'visible');
+}, 755);
+
+/*
+$( ".navbar-dropdown-main" ).hover(
+  function() {
+      $(".dropdown-menu-title").animate({
+    height: "2.5em"
+  }, 500, function() { });
+  }, function() {
+     $(".dropdown-menu-title").animate({
+    height: "0px"
+  }, 500, function() { });
+  }
+);
+*/
 
 
 $(document).ready(function() {
